820e385e8aba0138a80b56cc08a85144365716ea
Bruno Heinrich <brunu1994@gmail.com>
2019-12-17 01:08:28 -0300

    Card creator test

--- src/scenes/stores/WeaponStore.js
+++ src/scenes/stores/WeaponStore.js
@@ -10,13 +10,23 @@ class WeaponStore extends Phaser.Scene {
 
     create() {
         this.add.image(400, 300, 'weapon_store_background');
+        this.add.text(50, 520, "Press Q to exit the shop", {fontFamily: "courier", color: "black"});
 
         goBackButton = this.input.keyboard.addKey('Q');
 
-        this.add.text(50, 520, "Press Q to exit the shop", {fontFamily: "courier", color: "black"});
+        storeItems = itemList.getItemsByGroup("weapons");
+        itemsSortedByPage = itemList.sortItemsByPage(storeItems);
     }
 
     update() {
         listenShopExit(175, 296, this);
+
+        if (!itemLoaded) {
+            createShopList(storeItems, this);
+            this.add.image(287, 300, "shop_card");
+            this.add.image(287, 350, "shop_card");
+
+            itemLoaded = true;
+        }
     }
 }
